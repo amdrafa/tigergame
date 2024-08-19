58d3501c48eaffc37864197cdecacea4c663db50
unknown <tmrafinha4@gmail.com>
2024-08-19 16:58:56 -0300

    sadjdasj

--- src/components/header-app/index.tsx
+++ src/components/header-app/index.tsx
@@ -1,11 +1,12 @@
 import { toast } from "react-toastify";
 import logo from "../../assets/tigergpt.png";
+import { useNavigate } from "react-router-dom";
 
 
 export function HeaderApp() {
 
     function notify() { toast.error("Você precisa estar em MODO AUTOMÁTICO pra sacar seu dinheiro."); }
-
+    const navigate = useNavigate();
 
 
 
@@ -15,7 +16,7 @@ export function HeaderApp() {
                 <img width={90} src={logo} alt="Logo Tiger GPT" />
                 <div className="flex gap-4 text-zinc-300">
                     <p className=" text-xl text-yellow-400 cursor-pointer">Home</p>
-                    <button className="cursor-pointer text-xl" onClick={notify}>Meus Saques</button>
+                    <button className="cursor-pointer text-xl" onClick={() => navigate('/saque')}>Meus Saques</button>
                 </div>
 
             </header>
